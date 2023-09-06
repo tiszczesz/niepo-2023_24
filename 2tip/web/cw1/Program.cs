ddd5fcbec01c44dedbd261456c3cb62a25b9cb57
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-09-06 13:30:26 +0200

    first

---
+++ 2tip/web/cw1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Witamy w klasie 2tip");
+Console.WriteLine("Jestem dumny z mojego kodu!!!!");
+Console.Write("fdfgd gdgdf gdfgdgd\n");
+Console.Write("---------");
+Console.Write("\n\t***************************************");
+Console.Write("\n\t*         Jan Nowak                   *");
+Console.Write("\n\t*         Rok Szkolny 2023/24         *");
+Console.Write("\n\t*         Klasa 2TIP                  *");
+Console.Write("\n\t***************************************\n");
+int a = 1233333333;
+long dd = 3423232323544444444;
+Console.WriteLine("a = " + a);
+Console.WriteLine("dd = " + dd);
+Console.WriteLine(dd+a);
+Console.WriteLine((dd*a)/a);
+
+//Console.ReadKey();
