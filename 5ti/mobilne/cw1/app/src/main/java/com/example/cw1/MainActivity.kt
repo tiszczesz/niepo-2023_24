9677d73e92e70c7a1cee62868e228248faaebed9
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-09-07 13:35:31 +0200

    gg

--- 5ti/mobilne/cw1/app/src/main/java/com/example/cw1/MainActivity.kt
+++ 5ti/mobilne/cw1/app/src/main/java/com/example/cw1/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.cw1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val secondText = findViewById<TextView>(R.id.toClick)
+        secondText.setOnClickListener {
+            
+        }
     }
 }
