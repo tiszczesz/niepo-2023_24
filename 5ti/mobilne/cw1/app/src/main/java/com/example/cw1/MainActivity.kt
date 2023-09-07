4ea2d8fbc18a5e521450b099ed93ff227c6bdb34
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-09-07 14:37:09 +0200

    gg

--- 5ti/mobilne/cw1/app/src/main/java/com/example/cw1/MainActivity.kt
+++ 5ti/mobilne/cw1/app/src/main/java/com/example/cw1/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val secondText = findViewById<TextView>(R.id.toClick)
         secondText.setOnClickListener {
-            
+            secondText.setText("Po kliknięciu.....")
         }
     }
 }
