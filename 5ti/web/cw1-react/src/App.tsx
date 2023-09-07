4b425c96f2d7042921a1908b8044d20a0dc053d1
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-09-07 09:49:56 +0200

    gg

---
+++ 5ti/web/cw1-react/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+     <h1>Hello from React</h1>
+     <p className='par1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum molestiae perferendis a quis, assumenda doloremque, tempore fuga quod placeat doloribus maiores soluta corporis, nobis ad necessitatibus tenetur repellat dolore hic?</p>
+    </div>
+  );
+}
+
+export default App;
