f4f73d498caaf2cc00226a1e23c7fb61a5128d16
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-09-08 13:35:16 +0200

    ggg

---
+++ 2tip/desk/cw1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string firstname;
+Console.Write("Podaj swoje imie: ");
+firstname = Console.ReadLine();
+Console.WriteLine($"Witaj {firstname}");
+
+int age = 0;
+Console.Write("Podaj swoj wiek: ");
+age = Convert.ToInt32( Console.ReadLine());
+Console.WriteLine("Witaj "+firstname+ " twoj wiek: "+age);
+Console.WriteLine($"Witaj {firstname} twoj wiek: {age}");
--- 5ti/web/cw1-react/src/components/MyComp1.tsx
+++ 5ti/web/cw1-react/src/components/MyComp1.tsx
@@ -1,15 +1,19 @@
+import Euklides from  '../media/Euklides.jpg'
+import './MyComp1.css';
 function MyComp1(){
     let a = 12;
     let b= 45;
-    
+    let author = "Alojzy Gąbka";
     return (
         <div>
             <h1>MyComp1 </h1>
             {`${a} + ${b} = ${a+b}`}
             <p>
                 Data: {new Date().toLocaleDateString()}<br />
-                Czas: {new Date().toLocaleTimeString()}<br />
+                Czas: {new Date().toLocaleTimeString()}
+                <img onClick={()=>{alert("Hello from Eukldes")}} className='obrazek' src={Euklides} alt="Euklides" />
             </p>
+            <div style={{color:'red',fontSize:'2em'}}>To jest inny div ostylowany {author}</div>
         </div>
     )
 }
