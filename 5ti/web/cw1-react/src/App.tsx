6e61d6350847ce64aa01695c7381413ec1cd3665
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-09-13 14:57:12 +0200

    mm

--- 5ti/web/cw1-react/src/App.tsx
+++ 5ti/web/cw1-react/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './App.css';
 import MyComp1 from './components/MyComp1';
+import MyList1 from './components/MyList1';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
      <h1>Hello from React</h1>
      <p className='par1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum molestiae perferendis a quis, assumenda doloremque, tempore fuga quod placeat doloribus maiores soluta corporis, nobis ad necessitatibus tenetur repellat dolore hic?</p>
     <MyComp1 />
+    <MyList1 />
     </div>
   );
 }
---
+++ 5ti/web/cw1-react/src/components/MyList1.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+type Props = {}
+
+const MyList1 = (props: Props) => {
+    const todos = [
+        "Przeczytać o map w javascript",
+        "Zastosować map do generowania listy todos",
+        "Ostylować listę",
+        "Pochwalić się lub nie z efektu pracy",
+        "zjeść porządnie"
+    ];
+    return (
+        <>
+            <div>Moja lista do zrobienia</div>
+            <ul style={{listStyleType:"circle"}}>
+                {todos.map((elem,index)=>(
+                    <li key={index}>{elem}</li>
+                ))}
+            </ul>
+        </>
+
+
+    )
+}
+
+export default MyList1
