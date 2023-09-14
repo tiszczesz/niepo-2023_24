e20d45d14083e65635b653306f7e72ceb7301a26
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-09-14 10:08:27 +0200

    ff

--- 5ti/web/cw2-react/src/components/MyRange.tsx
+++ 5ti/web/cw2-react/src/components/MyRange.tsx
@@ -3,16 +3,29 @@ import './MyRange.css';
 
 
 const MyRange = () => {
-    const [myValue, setMyValue] = useState('0');
+    const [myRed, setMyRed] = useState('0');
+    const [myGreen, setMyGreen] = useState('0');
+    const [myBlue, setMyBlue] = useState('0');
     return (
         <>
             <div className="row">
-                <input min="0" max="255" className='col-6 w-50' value={myValue} type='range' onChange={(event) => {
-                    setMyValue(event.target.value)
+                <input min="0" max="255" className='col-6 w-50' value={myRed} type='range' onChange={(event) => {
+                    setMyRed(event.target.value)
                 }} />
-                <div className='col-6'>{myValue}</div>
+                <div className='col-6' style={{color:"red"}}>Red: {myRed}</div>
             </div>
-
+            <div className="row">
+                <input min="0" max="255" className='col-6 w-50' value={myGreen} type='range' onChange={(event) => {
+                    setMyGreen(event.target.value)
+                }} />
+                <div className='col-6' style={{color:"green"}}>Green: {myGreen}</div>
+            </div>
+            <div className="row">
+                <input min="0" max="255" className='col-6 w-50' value={myBlue} type='range' onChange={(event) => {
+                    setMyBlue(event.target.value)
+                }} />
+                <div className='col-6' style={{color:"blue"}}>Blue: {myBlue}</div>
+            </div>
             <div id='scene' className='scene'></div>
         </>
 
