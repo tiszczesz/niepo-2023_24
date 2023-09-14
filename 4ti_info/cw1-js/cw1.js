5b6ad9f11c21878eec29511dba398bd06d6e1949
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-09-14 08:36:58 +0200

    gg

---
+++ 4ti_info/cw1-js/cw1.js
@@ -0,0 +1,21 @@
+const Book1 = {
+    //pola obiektu lub właściwości
+    title: "ABC programowania obiektowego w JS",
+    author: "Marian Zagórny",
+    price: 45.99,
+
+    //metody obiektu
+    show: function(){
+        return `
+            <h4>Tytuł: ${this.title}</h4>
+            <h5>Autor: ${this.author}</h5>
+            <h6>Cena: ${this.price} zł</h6>
+        `;
+    },
+    setPrice: function(price){
+        this.price = price;
+    }
+}
+const Film1 = {};
+document.querySelector("#p1").innerHTML = Book1.show();
+document.querySelector("#p2").innerHTML = Film1.show();
