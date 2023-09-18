68fea3c6fb3acbea823253f0ac0142dd356597c5
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-09-18 08:33:09 +0200

    dd

--- 4ti_info/cw1-js/cw1-ex1.js
+++ 4ti_info/cw1-js/cw1-ex1.js
@@ -1,8 +1,10 @@
-function Student(firstname,lastname,division){
+function Student(firstname, lastname, division) {
 
 }
-
-//pobranie danych z formularza i wpisanie do utworzonego obiektu
-//const s1 = new Student(......)
-//wyswitlenie pod formularzem
-// lub w consoli    console.log(s1)  bieda kod
+document.querySelector("#save").onclick = function () {
+    //pobranie danych z formularza i wpisanie do utworzonego obiektu
+    //const s1 = new Student(......)
+    //wyswitlenie pod formularzem
+    // lub w consoli    console.log(s1)  bieda kod
+    alert("tutaj realizacja kodu");
+}
