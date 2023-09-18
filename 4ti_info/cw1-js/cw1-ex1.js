b4b9ab55f4d5711c5725e41be0b0a3c9c575d878
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-09-18 08:21:45 +0200

    dd

---
+++ 4ti_info/cw1-js/cw1-ex1.js
@@ -0,0 +1,8 @@
+function Student(firstname,lastname,division){
+
+}
+
+//pobranie danych z formularza i wpisanie do utworzonego obiektu
+//const s1 = new Student(......)
+//wyswitlenie pod formularzem
+// lub w consoli    console.log(s1)  bieda kod
