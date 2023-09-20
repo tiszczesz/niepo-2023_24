b98cee7c5e61a19e1663260ec5f1afc6d0a5ec28
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-09-20 08:28:50 +0200

    gg

---
+++ 2tip/desk/cw2/Program.cs
@@ -0,0 +1,8 @@
+﻿//napisac apliacje zgadywanka 
+//losowanie liczby z przedziału
+
+Random rnd = new Random();
+int toFind = rnd.Next(0,50); // wylosowanie liczby 0..50
+//uzytkownik zgaduje ta liczbe aplikacja podowiada 
+// czy liczba jest za duza czy za mala jak zgadnie to gratulacje
+// zgadles za 7 razem
