f4b2423239f05afd7564160ea13bca0c9e494520
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-09-20 13:52:50 +0200

    ff

---
+++ 3ti/web/Cw2_Ark1/Cw2_Ark1/Controllers/WeatherController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cw2_Ark1.Controllers
+{
+    public class WeatherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 3ti/web/Cw2_Ark1/Cw2_Ark1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cw2_Ark1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
