acbf9f556adc4ce286eeb1391de6266884efced2
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-09-27 08:45:24 +0200

    gg

---
+++ 2tip/desk/cw3/Person.cs
@@ -0,0 +1,22 @@
+﻿namespace cw3;
+
+public class Person
+{
+    private string firstname;
+    private string lastname;
+    private DateTime birthDay;
+    public Person(string firstname,string lastname,DateTime birthDay)
+    {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.birthDay = birthDay;
+    }
+    public Person(){
+         this.firstname = "Adam";
+        this.lastname = "Kowalski";
+        this.birthDay = new DateTime(2000,1,1);
+    }
+    public void Show(){
+        Console.WriteLine($"Imie: {firstname} Nazwisko: {lastname} Data urodzenia: {birthDay.ToShortDateString()}");
+    }
+}
---
+++ 2tip/desk/cw3/Program.cs
@@ -0,0 +1,6 @@
+﻿using cw3;
+
+Person p1 = new Person("Adam","Nowak",new DateTime(2005,6,12));
+Person p2 = new Person("Renata","Nowak",new DateTime(2007,12,24));
+p1.Show();
+p2.Show();
