1102a4ca989db62edb2a2bb83e58774687c14d91
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-09-28 08:45:12 +0200

    gg

---
+++ 4ti_info/cw3_jQuery/cw3.js
@@ -0,0 +1,12 @@
+$(function () {
+    const p1 = $("#p1");
+    const p11 = document.querySelector("#p1");
+
+    console.log(p1[0]);
+    //p1.style.color = "red";
+    p1[0].style.color = "green";
+    //p11.style.color = "red";
+    console.log(p11);
+});
+
+
