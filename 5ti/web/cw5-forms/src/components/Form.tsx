296514fdcd42a1877f89d17b9f83cf5037fc2e3f
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-09-28 09:20:35 +0200

    gg

--- 5ti/web/cw5-forms/src/components/Form.tsx
+++ 5ti/web/cw5-forms/src/components/Form.tsx
@@ -1,47 +1,56 @@
-import React, { FormEvent,useRef } from 'react'
+import React, { FormEvent, useRef } from 'react'
 
 type Props = {}
 
 const Form = (props: Props) => {
-    
-    const student = {firstname:'',lastname:'',age:0,date:new Date()}
+
+    const student = { firstname: '', lastname: '', age: 0, date: new Date() }
     const firstnameRef = useRef<HTMLInputElement>(null);
     const lastnameRef = useRef<HTMLInputElement>(null);
     const ageRef = useRef<HTMLInputElement>(null);
     const dateRef = useRef<HTMLInputElement>(null);
     function handleSubmit(event: FormEvent<HTMLFormElement>): void {
         event.preventDefault();
-        if(
+        if (
             firstnameRef.current !== null &&
-            lastnameRef.current !== null  &&
-            ageRef.current !== null  &&
-            dateRef.current !== null ){
-                
-                
-            }
+            lastnameRef.current !== null &&
+            ageRef.current !== null &&
+            dateRef.current !== null) {
+            student.firstname = firstnameRef.current.value;
+            student.lastname = lastnameRef.current.value;
+            student.age = parseInt(ageRef.current.value);
+            student.date = new Date(dateRef.current.value);
+            console.log(student);
+            (document.querySelector("#result") as HTMLDivElement)
+                .innerHTML = `imię ${student.firstname} nazwisko: ${student.lastname}
+                    wiek: ${student.age} data rejestracji: ${student.date.toLocaleDateString()}`
+        }
     }
 
-  return (
-    <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-            <label htmlFor="" className="form-label">Imię</label>
-            <input ref={firstnameRef} type="text" className="form-control" />
-        </div>
-        <div className="mb-3">
-            <label htmlFor="" className="form-label">Nazwisko</label>
-            <input ref={lastnameRef} type="text" className="form-control" />
-        </div>
-        <div className="mb-3">
-            <label htmlFor="" className="form-label">Wiek</label>
-            <input ref={ageRef} type="number" className="form-control" />
-        </div>
-        <div className="mb-3">
-            <label htmlFor="" className="form-label">Data rejestracji</label>
-            <input ref={dateRef} type="date" className="form-control" />
-        </div>
-        <button className='btn btn-primary w-100' type='submit'>Zapisz</button>
-    </form>
-  )
+    return (<div className='d-flex justify-content-around'>
+        <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+                <label htmlFor="" className="form-label">Imię</label>
+                <input ref={firstnameRef} type="text" className="form-control" />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="" className="form-label">Nazwisko</label>
+                <input ref={lastnameRef} type="text" className="form-control" />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="" className="form-label">Wiek</label>
+                <input ref={ageRef} type="number" className="form-control" />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="" className="form-label">Data rejestracji</label>
+                <input ref={dateRef} type="date" className="form-control" />
+            </div>
+            <button className='btn btn-primary w-100' type='submit'>Zapisz</button>
+        </form>
+        <div id="result"></div>
+    </div>
+
+    )
 }
 
 export default Form
