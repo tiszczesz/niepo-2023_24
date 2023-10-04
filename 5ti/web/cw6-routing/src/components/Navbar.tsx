ae4f45769cb37ae9152e3fbe1c881a6c3c806082
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-10-04 14:57:20 +0200

    hh

--- 5ti/web/cw6-routing/src/components/Navbar.tsx
+++ 5ti/web/cw6-routing/src/components/Navbar.tsx
@@ -3,13 +3,24 @@ import { Link } from "react-router-dom"
 
 const Navbar = () => {
     return (
-        <nav className="navbar bg-body-tertiary">
-            <div className="container-fluid">
-               <Link to='/' className="navbar-brand">
-                Strona główna
-               </Link>
-            </div>
-        </nav>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Strona domowa</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">             
+              <li className="nav-item">
+                <Link className="nav-link" to="/cw1">ćwiczenie 1</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/cw2">ćwiczenie 2</Link>
+              </li>            
+            </ul>
+          </div>
+        </div>
+      </nav>
     )
 }
 
