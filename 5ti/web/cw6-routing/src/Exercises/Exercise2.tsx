1b33ec4edc704e98637b18a9a85dac0b1749b896
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-10-05 10:02:45 +0200

    hh

--- 5ti/web/cw6-routing/src/Exercises/Exercise2.tsx
+++ 5ti/web/cw6-routing/src/Exercises/Exercise2.tsx
@@ -1,10 +1,22 @@
-
-
-
+import { useEffect, useState } from "react";
 
 const Exercise2 = () => {
+  const [products,setProducts] = useState<string[]>([]);
+  useEffect(()=>{
+      console.log("pobieranie produktów...");
+      setProducts(['Ryby','Bułki']);
+      
+  },[]);
   return (
-    <div>Exercise2</div>
+    <> 
+    <h3>Lista produktów</h3>
+    <ul>
+      {products.map((elem,index)=>(
+        <li key={index}>{elem}</li>
+      ))}
+    </ul>
+    </>
+   
   )
 }
 
