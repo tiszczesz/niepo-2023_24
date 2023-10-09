e777c00fa7fe6d8da61816ec8a0f476508766fd1
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-10-09 10:06:18 +0200

    ff

---
+++ 2tip/web/cw5/Program.cs
@@ -0,0 +1,8 @@
+﻿//Zdefiniowac klase Book z polami prywatnymi title author price
+//w pliku Book.cs
+//dorobic property get set tak aby cena byla dodatnia
+
+//Stworzyc liste ksiazek
+//List<Book> books = new List<Book>();
+// books.Add(new Book(.....))
+//dodac kilka ksiazek i wyswietlic wszystki foreach-em
