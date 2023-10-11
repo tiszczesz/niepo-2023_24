a9f1f2e9e5e5df3af9f0a05a684a624b7d4de17f
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-10-11 14:25:02 +0200

    gg

---
+++ 3ti/web/MVC_Courses/MVC_Courses/Controllers/CoursesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Courses.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
