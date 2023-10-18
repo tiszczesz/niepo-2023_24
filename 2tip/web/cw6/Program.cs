c8225806b0898bdba2991c3257b70479e497fdb8
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-10-18 12:02:46 +0200

    gg

---
+++ 2tip/web/cw6/Program.cs
@@ -0,0 +1,8 @@
+﻿// aplikacja z menu  do ... while
+// 1 - dodaj nowy film title, director, price, length, year, language/s
+//    dodawanie oboektu film do listy
+//      zapis do pliku
+// 2 - wyswietl wszystkie filmy popbranie z pliku
+// 3 -  zakoncz program   ----  ambitnie moga dorobic usuwanie filmu
+// title;director;price;length;year;language/s
+// title;director;price;length;year;language/s
