6ce158bccbca0c9ed281f9db7d1af82e63502cb3
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-10-18 08:56:21 +0200

    gg

---
+++ 2tip/desk/cw4/Program.cs
@@ -0,0 +1,27 @@
+﻿Ex1();
+Ex2();
+
+void Ex2()
+{
+   //Napisac aplikacje która prosi użytkownika o podawanie 
+   //kolejnych lini tekstu
+   //aż do momentu napisania koniec
+   //wszystko zapisuje sie do List<string>
+   //potem zapis do pliku
+   //następnie odczyt z pliku
+}
+
+void Ex1()
+{
+    List<string> words = new List<string>{
+        "Ala ma kota",
+        "druga linijka tekstu",
+        "i jeszcze coś ciekawego"
+    };
+    File.WriteAllLines("data2.txt", words);
+    var wiadro = File.ReadAllLines("data.txt");
+    foreach (string line in wiadro)
+    {
+        Console.WriteLine(line);
+    }
+}
