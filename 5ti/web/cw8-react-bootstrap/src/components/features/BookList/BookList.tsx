4fd64cc9180a285e20838bdc1d67a36026c56961
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-10-26 10:37:01 +0200

    hh

--- 5ti/web/cw8-react-bootstrap/src/components/features/BookList/BookList.tsx
+++ 5ti/web/cw8-react-bootstrap/src/components/features/BookList/BookList.tsx
@@ -1,14 +1,29 @@
 
 import React from 'react'
 import { Book } from '../../../interfaces/book'
+import { Col, ListGroup, ListGroupItem, Row } from 'react-bootstrap'
 
 type BookListProps = {
   books:Book[],
 }
 
-const BookList = (props: BookListProps) => {
+const BookList = ({books}: BookListProps) => {
   return (
-    <div>BookList</div>
+    <div>
+      <h2>Lista książek</h2>
+      <ListGroup className='mt-4'>
+        {books.map((book =>(
+          <ListGroup.Item className='bg-light' key={book.id}>
+            <Row>
+              <Col md={6}>
+                {book.title} {book.author} {book.price} zł
+              </Col>
+              <Col></Col>
+            </Row>
+          </ListGroup.Item>
+        )))}
+      </ListGroup>
+    </div>
   )
 }
 
