d509ea4392ce90c8403d580497ac337d0b410905
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-11-03 09:51:39 +0100

    hh

---
+++ 4ti_prog/web/ark2/solution/solution/Controllers/AnimalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace solution.Controllers
+{
+    public class AnimalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
