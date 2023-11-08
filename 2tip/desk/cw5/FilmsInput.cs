920b61398304e1e8afa6c118964f2763ddef6097
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-11-08 08:29:05 +0100

    kk

---
+++ 2tip/desk/cw5/FilmsInput.cs
@@ -0,0 +1,23 @@
+﻿namespace cw5;
+
+public class FilmsInput
+{
+    public FilmsInput(List<Film> films)
+    {
+        Films = films;
+    }
+    public List<Film> Films { get; set; }
+    public void ShowAllFilms(){
+        Console.WriteLine(" ================ Wyswietlanie filmow ========");
+        foreach(var film in Films){
+            ShowFilm(film);
+        }
+    }
+    public void ShowFilm(Film film){
+        Console.WriteLine(" Informacje o filmie: ");
+        Console.WriteLine($"\tTytul: {film.Title}");
+        Console.WriteLine($"\tRezyser: {film.Director}");
+        Console.WriteLine($"\tCena: {film.Price} zł");
+        Console.WriteLine($"\tOpis: {film.Description}");
+    }
+}
