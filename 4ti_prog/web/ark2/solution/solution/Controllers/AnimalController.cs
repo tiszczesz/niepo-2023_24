6f87dca1c7e9cfe11dcb834636121acbcdaf29e8
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-11-10 15:17:16 +0100

    tt

--- 4ti_prog/web/ark2/solution/solution/Controllers/AnimalController.cs
+++ 4ti_prog/web/ark2/solution/solution/Controllers/AnimalController.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using solution.Models;
 
 namespace solution.Controllers
 {
-    public class AnimalController : Controller
-    {
+    public class AnimalController : Controller {
+        private AnimalsDb db;
 
-        public IActionResult Index()
-        {
-            return View();
+        public AnimalController(IConfiguration config) {
+            db = new AnimalsDb(config.GetConnectionString("mysqlConn"));
+        }
+        public IActionResult Index() {
+            List<Animal> animals = db.GetAllAnimals();
+            return View(animals);
         }
     }
 }
--- 4ti_prog/web/ark2/solution/solution/Models/AnimalsDb.cs
+++ 4ti_prog/web/ark2/solution/solution/Models/AnimalsDb.cs
@@ -13,12 +13,42 @@ namespace solution.Models
 
         public List<Animal> GetAllAnimals() {
             List<Animal> animals = new List<Animal>();
+            using (MySqlConnection conn = new MySqlConnection(this.connString)) {
+                MySqlCommand command = conn.CreateCommand();
+                command.CommandText = "SELECT id,Gromady_id,Odzywianie_id,gatunek,wystepowanie, "
+                                      + "czy_zagrozony,obraz FROM zwierzeta";
+                conn.Open();
+                MySqlDataReader rd = command.ExecuteReader();
+                while (rd.Read()) {
+                    animals.Add(new Animal {
+                        Id = rd.GetInt32(0),
+                        AnimalClass_id = rd.GetInt32(1),
+                        Food_id = rd.GetInt32(2),
+                        Genre = rd.GetString(3),
+                        Place = rd.GetString(4),
+                        IsDanger = rd.GetBoolean(5),
+                        ImageName = rd.GetString(6)
+                    });
+                }
+                conn.Close();
+            }
+            return animals;
+        }
+
+        public List<AnimalClass> GetAnimalClasses() {
+            List<AnimalClass> animalClasses = new List<AnimalClass>();
             using (MySqlConnection conn = new MySqlConnection(this.connString))
             {
-
+                MySqlCommand command = conn.CreateCommand();
+                command.CommandText = "";
+                conn.Open();
+                MySqlDataReader rd = command.ExecuteReader();
+                while (rd.Read())
+                {
+                }
+                conn.Close();
             }
-
-            return animals;
+            return animalClasses;
         }
     }
 
