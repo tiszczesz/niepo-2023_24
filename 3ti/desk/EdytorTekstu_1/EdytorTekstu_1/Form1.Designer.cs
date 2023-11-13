72ff5a394427e7948a9a45bfa4f08b029cfdb431
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-11-13 11:10:22 +0100

    ff

---
+++ 3ti/desk/EdytorTekstu_1/EdytorTekstu_1/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace EdytorTekstu_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            textBox1 = new TextBox();
+            menuStrip1 = new MenuStrip();
+            plikToolStripMenuItem = new ToolStripMenuItem();
+            odczytToolStripMenuItem = new ToolStripMenuItem();
+            zapisToolStripMenuItem = new ToolStripMenuItem();
+            infoToolStripMenuItem = new ToolStripMenuItem();
+            statusStrip1 = new StatusStrip();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BorderStyle = BorderStyle.Fixed3D;
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 24);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(200, 426);
+            panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Dock = DockStyle.Fill;
+            textBox1.Location = new Point(200, 24);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(600, 404);
+            textBox1.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { plikToolStripMenuItem, infoToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 2;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // plikToolStripMenuItem
+            // 
+            plikToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { odczytToolStripMenuItem, zapisToolStripMenuItem });
+            plikToolStripMenuItem.Name = "plikToolStripMenuItem";
+            plikToolStripMenuItem.Size = new Size(38, 20);
+            plikToolStripMenuItem.Text = "Plik";
+            // 
+            // odczytToolStripMenuItem
+            // 
+            odczytToolStripMenuItem.Name = "odczytToolStripMenuItem";
+            odczytToolStripMenuItem.Size = new Size(109, 22);
+            odczytToolStripMenuItem.Text = "odczyt";
+            // 
+            // zapisToolStripMenuItem
+            // 
+            zapisToolStripMenuItem.Name = "zapisToolStripMenuItem";
+            zapisToolStripMenuItem.Size = new Size(109, 22);
+            zapisToolStripMenuItem.Text = "Zapis";
+            // 
+            // infoToolStripMenuItem
+            // 
+            infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            infoToolStripMenuItem.Size = new Size(40, 20);
+            infoToolStripMenuItem.Text = "Info";
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Location = new Point(200, 428);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(600, 22);
+            statusStrip1.TabIndex = 0;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(statusStrip1);
+            Controls.Add(panel1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private TextBox textBox1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem plikToolStripMenuItem;
+        private ToolStripMenuItem odczytToolStripMenuItem;
+        private ToolStripMenuItem zapisToolStripMenuItem;
+        private ToolStripMenuItem infoToolStripMenuItem;
+        private StatusStrip statusStrip1;
+    }
+}
---
+++ 3ti/desk/EdytorTekstu_1/EdytorTekstu_1/Form1.cs
@@ -0,0 +1,10 @@
+namespace EdytorTekstu_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
