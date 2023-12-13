f75159dd87fefea78ae470aa6a66d4cda06c70d2
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-12-13 11:07:13 +0100

    ff

---
+++ 2tip/web/cw9/cw9_razor_pages_empty/wwwroot/index1.js
@@ -0,0 +1,4 @@
+setTimeout(()=>{
+    document.querySelector("#root")
+    .innerHTML = new Date().toLocaleDateString()
+},2000);
--- 4ti_prog/desktopowe/cw2_java_gui/cw2_java_gui/src/MainWindow.java
+++ 4ti_prog/desktopowe/cw2_java_gui/cw2_java_gui/src/MainWindow.java
@@ -1,2 +1,56 @@
-package PACKAGE_NAME;public class MainWindow {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.LocalDate;
+
+
+public class MainWindow extends JFrame {
+    private JButton button1;
+    private JPanel jPanel1;
+    private JLabel jLabel1;
+    private JTextField textField1;
+    private JButton changeLabelButton;
+    private JButton wyjścieButton;
+    private JButton pokażDatęButton;
+
+    public MainWindow()
+    {
+        setTitle("Pierwsza App GUI");
+        setContentPane(jPanel1);
+        setSize(400,400);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jLabel1.setText("Hello from SWING GUI");
+            }
+        });
+        changeLabelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(textField1.getText().trim().isEmpty()){
+                    JOptionPane.showMessageDialog(jPanel1,"Brak danych");
+                    return;
+                }
+                jLabel1.setText(textField1.getText());
+            }
+        });
+        wyjścieButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        pokażDatęButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(
+                        jPanel1,"Dzisiaj mamy: "+ LocalDate.now());
+            }
+        });
+    }
+    public static void main(String[] args){
+        new MainWindow();
+    }
 }
