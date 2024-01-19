90d25546869e467c8c548ddb4a8671dff59b45f4
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-01-19 13:46:57 +0100

    gg

--- 2tip/desk/WinForms_cw2/WinForms_cw2/Form1.Designer.cs
+++ 2tip/desk/WinForms_cw2/WinForms_cw2/Form1.Designer.cs
@@ -28,12 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            panel1 = new Panel();
+            btnInsertColor = new Button();
+            btnClearList = new Button();
+            btnAddColor = new Button();
+            cbColors = new ComboBox();
+            label2 = new Label();
+            listColors = new ListBox();
+            label1 = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.InactiveCaption;
+            panel1.Controls.Add(btnInsertColor);
+            panel1.Controls.Add(btnClearList);
+            panel1.Controls.Add(btnAddColor);
+            panel1.Controls.Add(cbColors);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(listColors);
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(581, 450);
+            panel1.TabIndex = 0;
+            // 
+            // btnInsertColor
+            // 
+            btnInsertColor.Enabled = false;
+            btnInsertColor.Location = new Point(397, 91);
+            btnInsertColor.Name = "btnInsertColor";
+            btnInsertColor.Size = new Size(149, 23);
+            btnInsertColor.TabIndex = 6;
+            btnInsertColor.Text = "Wstaw kolor";
+            btnInsertColor.UseVisualStyleBackColor = true;
+            // 
+            // btnClearList
+            // 
+            btnClearList.Location = new Point(397, 127);
+            btnClearList.Name = "btnClearList";
+            btnClearList.Size = new Size(149, 23);
+            btnClearList.TabIndex = 5;
+            btnClearList.Text = "Wyczyść listę kolorów";
+            btnClearList.UseVisualStyleBackColor = true;
+            btnClearList.Click += btnClearList_Click;
+            // 
+            // btnAddColor
+            // 
+            btnAddColor.Location = new Point(397, 55);
+            btnAddColor.Name = "btnAddColor";
+            btnAddColor.Size = new Size(149, 23);
+            btnAddColor.TabIndex = 4;
+            btnAddColor.Text = "Dodaj kolor";
+            btnAddColor.UseVisualStyleBackColor = true;
+            btnAddColor.Click += btnAddColor_Click;
+            // 
+            // cbColors
+            // 
+            cbColors.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbColors.FormattingEnabled = true;
+            cbColors.Items.AddRange(new object[] { "red", "green", "blue", "yellow", "purple", "white", "grey", "pink" });
+            cbColors.Location = new Point(270, 56);
+            cbColors.Name = "cbColors";
+            cbColors.Size = new Size(121, 23);
+            cbColors.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(179, 58);
+            label2.Name = "label2";
+            label2.Size = new Size(79, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Wybierz kolor";
+            // 
+            // listColors
+            // 
+            listColors.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            listColors.FormattingEnabled = true;
+            listColors.ItemHeight = 15;
+            listColors.Location = new Point(12, 58);
+            listColors.Name = "listColors";
+            listColors.Size = new Size(161, 379);
+            listColors.TabIndex = 1;
+            
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label1.Location = new Point(68, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(166, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Zbiór kolorów";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Okno główne";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private ListBox listColors;
+        private Label label1;
+        private ComboBox cbColors;
+        private Label label2;
+        private Button btnAddColor;
+        private Button btnClearList;
+        private Button btnInsertColor;
     }
 }
--- 2tip/desk/WinForms_cw2/WinForms_cw2/Form1.cs
+++ 2tip/desk/WinForms_cw2/WinForms_cw2/Form1.cs
@@ -6,5 +6,27 @@ namespace WinForms_cw2
         {
             InitializeComponent();
         }
+
+        private void btnAddColor_Click(object sender, EventArgs e)
+        {
+            string? currentColor = cbColors.SelectedItem as string;
+            if (currentColor != null)
+            {
+                listColors.Items.Add(currentColor);
+               
+                // MessageBox.Show(listColors.DataSource)
+            }
+            else
+            {
+                MessageBox.Show("Wybierz kolor!!!");
+            }
+        }
+
+        private void btnClearList_Click(object sender, EventArgs e)
+        {
+            listColors.Items.Clear();
+        }
+
+     
     }
 }
