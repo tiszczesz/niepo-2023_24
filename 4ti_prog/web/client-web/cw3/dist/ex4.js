3475448b1008239adb2910ff320de4548ec66a78
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-01-24 11:23:01 +0100

    gg

---
+++ 4ti_prog/web/client-web/cw3/dist/ex4.js
@@ -0,0 +1,10 @@
+import { createInterface } from "readline/promises";
+const r1 = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+const answer1 = await r1.question("Podaj n: ");
+const answer2 = await r1.question("Podaj n2: ");
+console.log(" Podane n: ", answer1);
+console.log(" Podane 2: ", answer2);
+r1.close();
