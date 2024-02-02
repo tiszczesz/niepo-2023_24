78a50e91f6a811f8d28ff6e08c65f98b628310ba
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-02-02 10:35:13 +0100

    hh

---
+++ 3ti/web/cw3-fromEmpty/Controllers/PersonController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cw3_fromEmpty.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
