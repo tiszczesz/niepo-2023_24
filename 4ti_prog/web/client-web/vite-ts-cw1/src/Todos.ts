6f65ad8e1f38c1a1c0319867531ca5b048cb83fe
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-02-28 11:47:36 +0100

    hh

---
+++ 4ti_prog/web/client-web/vite-ts-cw1/src/Todos.ts
@@ -0,0 +1,11 @@
+export  type Todo = {
+    title:string;
+    content:string;
+    isComplete:boolean;
+}
+export const Todos:Todo[] = [
+    {title:"zadanie 1",content:"Zrobić na nie bańkę",isComplete:false},
+    {title:"zadanie 2",content:"Zrobić na nie bańkę",isComplete:true},
+    {title:"zadanie 3",content:"Zrobić na nie bańkę",isComplete:false},
+    {title:"zadanie 4",content:"Zrobić na nie bańkę",isComplete:false},
+];
--- 4ti_prog/web/client-web/vite-ts-cw1/src/main.ts
+++ 4ti_prog/web/client-web/vite-ts-cw1/src/main.ts
@@ -1,6 +1,7 @@
 import './style.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { Todo, Todos } from './Todos';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div class="container">
@@ -12,4 +13,20 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 const div = document.createElement("div");
 const text = document.createTextNode("Ala ma kota");
 div.appendChild(text);
+
 document.querySelector<HTMLDivElement>("#root")?.appendChild(div);
+document.querySelector<HTMLDivElement>("#root")?.appendChild(generList(Todos));
+function generList(data:Todo[]):HTMLDivElement{
+  const result = document.createElement("div");
+  const ul = document.createElement("ul");
+  ul.className = "list-group";
+  data.forEach((item,i)=>{
+    const li = document.createElement("li");
+    li.className = "list-group-item";
+    if(item.isComplete) li.style.color="green"; 
+    li.innerHTML = `<b>${item.title}</b> <i>${item.content}</i>`;
+    ul.appendChild(li);
+  })
+  result.appendChild(ul);
+  return result;
+}
