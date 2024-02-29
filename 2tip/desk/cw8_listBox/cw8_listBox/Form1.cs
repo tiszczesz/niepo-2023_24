51954504c9b689826e23b62e52ead4749e71a64b
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-02-29 13:00:08 +0100

    hhh

--- 2tip/desk/cw8_listBox/cw8_listBox/Form1.cs
+++ 2tip/desk/cw8_listBox/cw8_listBox/Form1.cs
@@ -5,28 +5,28 @@ namespace cw8_listBox
     public partial class Form1 : Form
     {
         private IRepository _repository;
+        public List<Product> Products { get; set; }
         public Form1()
         {
             InitializeComponent();
             _repository = new FakeRepo();
+            Products = _repository.GetProducts();
         }
 
-        private void btnLoad_Click(object sender, EventArgs e)
-        {
-            lbProducts.DataSource = _repository.GetProducts();
+        private void btnLoad_Click(object sender, EventArgs e) {
+            lbProducts.DataSource = Products;
         }
 
         private void lbProducts_DataSourceChanged(object sender, EventArgs e)
         {
-            lbCoumt.Text = _repository.GetProducts().Count.ToString();
+            lbCoumt.Text = Products.Count.ToString();
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            _repository.GetProducts()
-                .Add(new Product{Id = 6,Name = "ssss"});
+            Products.Add(new Product{Id = 6,Name = "ssss",Price = 23.89m});
             lbProducts.DataSource = null;
-            lbProducts.DataSource = _repository.GetProducts();
+            lbProducts.DataSource = Products;
         }
     }
 }
