290faa221755cb95fed0c1d91464cf4219c62686
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-03-04 10:36:11 +0100

    gg

---
+++ 2tip/web/WebEmptyFile/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace WebEmptyFile.Models
+{
+    public class Person
+    {
+        public Person() {
+            Id = new Guid().ToString();
+        }
+        public string Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Content { get; set; }
+        public DateOnly PartyDate { get; set; }
+    }
+}
