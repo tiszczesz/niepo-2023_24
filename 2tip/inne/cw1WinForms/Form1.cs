83226ef8645d3ce67037ee1acc9734885310dbe1
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-03-07 09:28:53 +0100

    dd

--- 2tip/inne/cw1WinForms/Form1.cs
+++ 2tip/inne/cw1WinForms/Form1.cs
@@ -2,14 +2,34 @@ namespace cw1WinForms
 {
     public partial class Form1 : Form
     {
+        private bool isStart = false;
         public Form1()
         {
             InitializeComponent();
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {
+        { 
+            timer1.Interval = 1000;
+            if (!isStart)
+            {
+                timer1.Start();
+                button1.Text = "STOP";
+            }
+            else
+            {
+                timer1.Stop();
+                button1.Text = "START";
+            }
+            
+            isStart = !isStart;
+        }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //label1.Text = DateTime.Now.ToString("HH:mm:ss");
+            label1.Text = DateTime.Now.ToLongTimeString();
         }
     }
 }
