79a1ceaabd0f08efed13b51e906aad26e0ad590d
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-04-03 08:18:51 +0200

    ff

---
+++ 2tip/desk/WpfApp1/ProductRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class ProductRepo
+    {
+        public static List<Product> GetProducts() {
+            return new List<Product>() {
+                new Product() { Id = 1, Name = "aaaa", Price = 23.89m, Description = "adas dasa" },
+                new Product() { Id = 2, Name = "ggg", Price = 23.89m, Description = "adas dasa" },
+                new Product() { Id = 3, Name = "fff", Price = 13.89m, Description = "adas dasa" },
+                new Product() { Id = 4, Name = "fff", Price = 44.89m, Description = "adas dasa" },
+            };
+        }
+    }
+}
