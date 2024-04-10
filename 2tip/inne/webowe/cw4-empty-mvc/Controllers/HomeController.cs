58e7d19b000399f538dd5d6d418fb4ebaa1cee0c
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-04-10 17:54:10 +0200

    ff

---
+++ 2tip/inne/webowe/cw4-empty-mvc/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cw4_empty_mvc.Controllers;
+
+public class HomeController : Controller
+{
+    public string Index(){
+        return "Hello from Index in Home";
+    }
+    public IActionResult Index2(){
+        return View();
+    }
+
+}
