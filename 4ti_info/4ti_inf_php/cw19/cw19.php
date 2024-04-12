6c134551317cbf608ddb9082622930aad384cb98
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-04-12 08:58:18 +0200

    Update cw19.php

--- 4ti_info/4ti_inf_php/cw19/cw19.php
+++ 4ti_info/4ti_inf_php/cw19/cw19.php
@@ -1,16 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <pre>
     <?php
-    $conn = new mysqli("localhost","root",null,"test");
-    var_dump($conn);
+    $conn = new mysqli("localhost", "root", null, "4ti_inf_2024_cw1");
+    $conn->set_charset("utf8"); //ustawienie kodowania połączenia
+    if ($conn->connect_errno !== 0)
+        die("Błąd połączenia: " . $conn->connect_errno); //zakonczenie skryptu gdy bład połaczenia z DB
+    $sqlQuery = "SELECT firstname,lastname,division FROM students ";
+    $result = $conn->query($sqlQuery);
+    var_dump($result);
+    //$data = $result->fetch_all();
+    //fetch_row() kilka razy 
+    $row = $result->fetch_row();
+    var_dump($row);
+    $row = $result->fetch_row();
+    var_dump($row);
+    $row = $result->fetch_row();
+    var_dump($row);
+    $row = $result->fetch_row();
+    var_dump($row);
+    $row = $result->fetch_row();
+    var_dump($row);
+    $row = $result->fetch_row();
+    var_dump($row);
+    $row = $result->fetch_row();
+    var_dump($row);
+    $row = $result->fetch_row();
+    var_dump($row);
+    $row = $result->fetch_row();
+    var_dump($row);
+    $row = $result->fetch_row();
+    var_dump($row);
+    $row = $result->fetch_row();
+    var_dump($row);
+
+    $conn->close();//!!!!!!!
     ?>
     </pre>
 </body>
+
 </html>
