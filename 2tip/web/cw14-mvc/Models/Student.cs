6a4071b99eeafab9fa8ccc2c0fb4168d68b9d252
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-04-22 10:19:19 +0200

    hh

---
+++ 2tip/web/cw14-mvc/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace cw14_mvc.Models;
+
+public class Student
+{
+    public int Id { get; set;}
+    
+    [DisplayName("Podaj imię")]
+    [Required(ErrorMessage = "Musisz podać imię")]
+    public string? FirstName { get; set; }
+
+    [DisplayName("Podaj nazwisko")]
+    [Required(ErrorMessage = "Musisz podać nazwisko")]
+    public string? LastName { get; set; }
+    
+    [Required(ErrorMessage = "Podaj poprawny wiek")]
+    [DisplayName("Podaj wiek")]
+    public int? Age { get; set; }
+}
---
+++ 2tip/web/cw14-mvc/Models/StudentRepo.cs
@@ -0,0 +1,15 @@
+﻿namespace cw14_mvc.Models;
+
+public class StudentRepo
+{
+    public static List<Student> GetStudents(){
+        return new List<Student> { 
+            new Student {Id = 1,FirstName="Roman",LastName="Nowak",Age=23},
+            new Student {Id = 2,FirstName="Monika",LastName="Młyn",Age=12},
+            new Student {Id = 3,FirstName="Teresa",LastName="Redecka",Age=45},
+            new Student {Id = 4,FirstName="Jerzy",LastName="Kowalski",Age=32},
+            new Student {Id = 51,FirstName="Tomasz",LastName="Nowak",Age=19},
+            new Student {Id = 6,FirstName="Eryk",LastName="Nowak",Age=20}
+        };
+    }
+}
