f227a0db89f90c78fb714c3371fad32e5a0d4b7d
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-05-16 16:00:55 +0200

    hh

--- 4ti_prog/desktopowe/kotlin/cw1/cw1/src/Book.kt
+++ 4ti_prog/desktopowe/kotlin/cw1/cw1/src/Book.kt
@@ -1,4 +1,4 @@
-class Book(private val title:String,private val author:String,private val price:Double) {
+class Book(private val title:String,private val author:String,private val price:Double=0.0) {
     var fullInfo: String
         get() {
             return "Tytuł: ${this.title} autor: ${this.author} cena: ${this.price} zł"
@@ -7,7 +7,7 @@ class Book(private val title:String,private val author:String,private val price:
         println("Tworzenie obiektu Book");
         fullInfo = ""
     }
-    constructor():this("noname","noname",0.0){
+    constructor():this("noname","noname"){
         println("Konstruktor bez argumentów")
     }
 }
--- 4ti_prog/desktopowe/kotlin/cw1/cw1/src/cw5.kt
+++ 4ti_prog/desktopowe/kotlin/cw1/cw1/src/cw5.kt
@@ -1,6 +1,10 @@
-fun main(){
-   val b1 = Book("ABC Kotlina" ,"Roman Boman",23.90)
-    val  b2 = Book()
-   println(b1.fullInfo)
-   println(b2.fullInfo)
+fun main() {
+    val b1 = Book("ABC Kotlina", "Roman Boman", 23.90)
+    val b2 = Book()
+    val b3 = Book("ggggg", "tttt")
+    println(b1.fullInfo)
+    println(b2.fullInfo)
+    println(b3.fullInfo)
+    var a = 12
+    val abs = if (a > 0) a else -a;
 }
