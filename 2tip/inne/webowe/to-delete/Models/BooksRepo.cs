6be9088eefa0a596104f74f4f3eac1766b9df527
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2024-06-05 17:45:42 +0200

    hh

---
+++ 2tip/inne/webowe/to-delete/Models/BooksRepo.cs
@@ -0,0 +1,40 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace to_delete;
+
+public class BooksRepo
+{
+    private string? _connString;
+    public BooksRepo(IConfiguration configuration)
+    {
+        _connString = configuration.GetConnectionString("mysqlConn");
+    }
+    public List<Book> GetAll(){
+        var books = new List<Book>();
+        using (MySqlConnection connection = new MySqlConnection(_connString) )
+        {
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM books";
+            connection.Open();
+            MySqlDataReader rd = command.ExecuteReader();
+            while (rd.Read()){
+                books.Add(new Book{
+                    Id = rd.GetInt32(0),
+                    Title = rd.GetString(1),
+                    Author = rd.GetString(2),
+                    Price = rd.GetDecimal(3),
+                });
+            }
+        }
+        return books;        
+    }
+    public void Delete(int? id){
+         using (MySqlConnection connection = new MySqlConnection(_connString) )
+        {
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = $"DELETE FROM books WHERE id={id}";
+            connection.Open();
+            command.ExecuteNonQuery();
+        }
+    }
+}
